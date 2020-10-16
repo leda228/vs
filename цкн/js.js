8800f26dd88dd5036f8f2abd3a99a44a951cfbde
leda228 <72969734+leda228@users.noreply.github.com>
2020-10-16 10:51:08 +0200

    Add files via upload

---
+++ цкн/js.js
@@ -0,0 +1,27 @@
+$(document).ready( function(){
+	$('.add1').click(function () {
+		$('.search1').slideToggle(300);
+	})
+
+	$('.add2').click(function () {
+		$('.search2').slideToggle(300);
+	})
+
+let OpMem = [
+	
+];
+
+let VidCard = [
+
+];
+
+let Proc = [
+	{name: 'Intel Core i5 9400', Cores: '6', Flow: '6'},
+	{name: 'AMD Ryzen 7 2700X', Cores: '8', Flow: '16'},
+	{name: 'Intel Core i7 7700', Cores: '4', Flow: '8'}
+];
+
+
+
+
+});
